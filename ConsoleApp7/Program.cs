6e9cbea538f1ea8b2c9f3d6393d1ce725bc5a2db
Никита Федосеев <zveri132@gmail.com>
2020-06-21 20:08:09 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Health
+    {
+        private int Temp;
+        public int Temperature
+        {
+            get
+            {
+                return Temp;
+            }
+            set
+            {
+                if (value < 34 || value > 41)
+                    Console.WriteLine("Вы вышли за диапозон ");
+                else
+                    Console.WriteLine("Ваша температура составляет " + value);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите температуру от 34 до 41 ");
+            int NumTemp = Convert.ToInt32(Console.ReadLine());
+            Health newTem = new Health();
+            newTem.Temperature = NumTemp;
+            
+            Console.ReadKey();
+        }
+    }
+}
